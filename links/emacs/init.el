8a9fb0cba5d890ca1919c96ffda7e9e730b64f97
izpa <ispius.prime@gmail.com>
2023-07-13 08:29:40 +0300

    emacs goldenratio

--- links/emacs/init.el
+++ links/emacs/init.el
@@ -36,7 +36,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(vterm lsp-treemacs treemacs-perspective treemacs-magit treemacs-icons-dired treemacs-projectile treemacs-evil treemacs perspective all-the-icons lsp-mode company flycheck counsel cider solarized-theme ivy evil-collection use-package)))
+   '(golden-ratio vterm lsp-treemacs treemacs-perspective treemacs-magit treemacs-icons-dired treemacs-projectile treemacs-evil treemacs perspective all-the-icons lsp-mode company flycheck counsel cider solarized-theme ivy evil-collection use-package)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
---
+++ links/emacs/stuff/16-goldenratio.el
@@ -0,0 +1,12 @@
+;;; package --- Summary
+;;; Commentary:
+
+;;; Code:
+
+(use-package golden-ratio
+  :ensure t
+  :init
+  (setq golden-ratio-auto-scale t)
+  :config
+  (golden-ratio-mode 1))
+;;; 16-goldenratio.el ends here
