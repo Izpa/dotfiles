a75cf74ddbec4f75fbc608e07c48d79e93ea7dc0
izpa <ispius.prime@gmail.com>
2023-07-18 09:30:00 +0300

    zsh + emacs vterm

--- links/emacs/stuff/15-vterm.el
+++ links/emacs/stuff/15-vterm.el
@@ -4,5 +4,12 @@
 ;;; Code:
 
 (use-package vterm
-    :ensure t)
+  :ensure t
+  :config
+  (evil-collection-vterm-setup)
+  (setq vterm-shell "zsh")
+  :custom
+  (vterm-environment '("TYPEWRITTEN_CURSOR=terminal"))
+  (vterm-ignore-blink-cursor t))
+
 ;;; 15-vterm.el ends here
