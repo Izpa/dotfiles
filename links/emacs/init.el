3d0541f5db6da41e8184a11a8cbe7a5516ff6b9e
izpa <ispius.prime@gmail.com>
2024-10-18 14:29:28 +0300

    add go for emacs and clear emacs configs

--- links/emacs/init.el
+++ links/emacs/init.el
@@ -34,8 +34,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(realgud pyenv-mode clojure-mode-extra-font-locking winum golden-ratio vterm lsp-treemacs treemacs-perspective treemacs-magit treemacs-icons-dired treemacs-projectile treemacs-evil treemacs perspective all-the-icons lsp-mode company flycheck counsel cider solarized-theme ivy evil-collection use-package)))
+ )
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
---
+++ links/emacs/stuff/22-go.el
@@ -0,0 +1,11 @@
+;;; package --- Summary
+;;; Commentary:
+
+;;; Code:
+
+(use-package go-mode
+  :ensure t)
+
+
+(add-to-list 'auto-mode-alist '("\\.go\\'" . go-mode))
+;;; 22-go.el ends here
